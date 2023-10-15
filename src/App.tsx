fdebd61020f9abff4579778c2e8aae689c8fd987
natgitinit <natgitinit@gmail.com>
2023-10-15 14:51:52 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import Home from './pages/home/Home'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "users",
+      element: <Users/>,
+    },
+    {
+      path: "products",
+      element: <Products/>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />
 }
 
 export default App
