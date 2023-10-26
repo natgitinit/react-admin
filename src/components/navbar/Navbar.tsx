544b1f57e67509d2ec6f85e08e2fb75edc33d70d
natgitinit <natgitinit@gmail.com>
2023-10-26 12:55:35 -0500

    adds user page and material ui chart

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,10 +2,10 @@ import "../navbar/navbar.scss";
 
 const Navbar = () => {
     return (
-        <div className="navbar"> Navbar 
+        <div className="navbar">
             <div className="logo">
                 <img src="logo.svg" alt=""/>
-                <span>lamadmin</span>
+                <span>Admin Dashboard</span>
             </div>
             <div className="icons">
                 <img src="/search.svg" alt="" className="icon" />
